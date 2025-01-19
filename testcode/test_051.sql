1a0a95158f14f9208b4659acf6e3e8186320c57f
Jooahyeon <dkgus0496@naver.com>
2025-01-20 03:07:30 +0900

    문의사항조회

--- testcode/test_051.sql
+++ testcode/test_051.sql
@@ -4,5 +4,5 @@ SELECT
      , a.admin_q_comment_time AS '등록시각'
      , a.admin_q_comment_content AS '답변내용'
 FROM admin_q_comment a
-         JOIN client b ON a.client_num = b.client_num;
+         JOIN client b ON a.client_num = b.client_num; 
 
