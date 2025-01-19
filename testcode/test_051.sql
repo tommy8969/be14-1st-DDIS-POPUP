284f82ad021fd15c8295f8ad8ca18e4f4bfc3cd4
1214alex <68007279+1214alex@users.noreply.github.com>
2025-01-20 03:05:28 +0900

    Create test_051.sql
    test_051

---
+++ testcode/test_051.sql
@@ -0,0 +1,7 @@
+-- Test_051 : 문의사항 답변조회
+SELECT
+    a.admin_q_comment_num AS '작성자'
+     , a.admin_q_comment_time AS '등록시각'
+     , a.admin_q_comment_content AS '답변내용'
+FROM admin_q_comment a
+         JOIN client b ON a.client_num = b.client_num;
